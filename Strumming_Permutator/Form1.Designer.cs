a61a2117d29e9c95901914fd8eabadf1a278d207
DC23 <jugglindan@gmail.com>
2019-08-04 10:04:48 +1000

    change form colour

--- Strumming_Permutator/Form1.Designer.cs
+++ Strumming_Permutator/Form1.Designer.cs
@@ -267,7 +267,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
             this.ClientSize = new System.Drawing.Size(743, 261);
             this.Controls.Add(this.tableLayoutPanel1);
             this.MinimumSize = new System.Drawing.Size(400, 300);
