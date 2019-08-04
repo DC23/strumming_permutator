1540de98cb79d4f4e2b90872314bd1ccff148c8a
DC23 <jugglindan@gmail.com>
2019-08-04 21:07:58 +1000

    fixing broken tests

--- Strumming_PermutatorTests/Form1Tests.cs
+++ Strumming_PermutatorTests/Form1Tests.cs
@@ -14,61 +14,65 @@ namespace Strumming_Permutator.Tests
         [TestMethod()]
         public void _4beats_1subdiv_0skip()
         {
-            Assert.AreEqual("1 2 3 4", Form1.GenerateSequence(4, 1));
+            Assert.AreEqual("1 2 3 4", Form1.GenerateSequence(4, 1).Item1);
+            Assert.AreEqual("1 2 3 4", Form1.GenerateSequence(4, 1).Item2);
         }
 
         [TestMethod]
         public void _4beats_2subdiv_0skip()
         {
-            Assert.AreEqual("1 + 2 + 3 + 4 +", Form1.GenerateSequence(4, 2));
+            Assert.AreEqual("1 + 2 + 3 + 4 +", Form1.GenerateSequence(4, 2).Item1);
+            Assert.AreEqual("1 + 2 + 3 + 4 +", Form1.GenerateSequence(4, 2).Item2);
         }
 
         [TestMethod]
         public void _4beats_3subdiv_0skip()
         {
-            Assert.AreEqual("1 + a 2 + a 3 + a 4 + a", Form1.GenerateSequence(4, 3));
+            Assert.AreEqual("1 + a 2 + a 3 + a 4 + a", Form1.GenerateSequence(4, 3).Item1);
+            Assert.AreEqual("1 + a 2 + a 3 + a 4 + a", Form1.GenerateSequence(4, 3).Item2);
         }
 
         [TestMethod]
         public void _4beats_4subdiv_0skip()
         {
-            Assert.AreEqual("1 e + a 2 e + a 3 e + a 4 e + a", Form1.GenerateSequence(4, 4));
+            Assert.AreEqual("1 e + a 2 e + a 3 e + a 4 e + a", Form1.GenerateSequence(4, 4).Item1);
+            Assert.AreEqual("1 e + a 2 e + a 3 e + a 4 e + a", Form1.GenerateSequence(4, 4).Item2);
         }
 
         [TestMethod()]
         public void _3beats_1subdiv_0skip()
         {
-            Assert.AreEqual("1 2 3", Form1.GenerateSequence(3, 1));
+            Assert.AreEqual("1 2 3", Form1.GenerateSequence(3, 1).Item1);
         }
 
         [TestMethod]
         public void _3beats_2subdiv_0skip()
         {
-            Assert.AreEqual("1 + 2 + 3 +", Form1.GenerateSequence(3, 2));
+            Assert.AreEqual("1 + 2 + 3 +", Form1.GenerateSequence(3, 2).Item1);
         }
 
         [TestMethod]
         public void _3beats_3subdiv_0skip()
         {
-            Assert.AreEqual("1 + a 2 + a 3 + a", Form1.GenerateSequence(3, 3));
+            Assert.AreEqual("1 + a 2 + a 3 + a", Form1.GenerateSequence(3, 3).Item1);
         }
 
         [TestMethod]
         public void _3beats_4subdiv_0skip()
         {
-            Assert.AreEqual("1 e + a 2 e + a 3 e + a", Form1.GenerateSequence(3, 4));
+            Assert.AreEqual("1 e + a 2 e + a 3 e + a", Form1.GenerateSequence(3, 4).Item1);
         }
 
         [TestMethod()]
         public void _6beats_1subdiv_0skip()
         {
-            Assert.AreEqual("1 2 3 4 5 6", Form1.GenerateSequence(6, 1));
+            Assert.AreEqual("1 2 3 4 5 6", Form1.GenerateSequence(6, 1).Item1);
         }
 
         [TestMethod]
         public void _6beats_2subdiv_0skip()
         {
-            Assert.AreEqual("1 + 2 + 3 + 4 + 5 + 6 +", Form1.GenerateSequence(6, 2));
+            Assert.AreEqual("1 + 2 + 3 + 4 + 5 + 6 +", Form1.GenerateSequence(6, 2).Item1);
         }
 
 
